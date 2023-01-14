b432814337d9fd14bd10eb2b5068f64b0f5be0c2
Tero Keränen <tero-keranen@hotmail.com>
2023-01-14 12:00:22 +0200

    Landing page proto

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
+let transition_el = document.querySelector('.transition')
+
+
+
+
+
+function delayRedirect () {
+    
+    
+    setInterval(function() {
+        
+        
+        
+        window.location = "http://127.0.0.1:5500/welcome.html"
+        
+
+        
+    },5000)
+    
+}
+
+
+// let transition_el = document.querySelector('.transition')
+function main () {
+    if (window.location.href === "http://127.0.0.1:5500/") {
+    transition_el.classList.remove('is-active');
+
+    window.onload = function () {
+        delayRedirect();
+        
+
+        }
+    }
+
+transition_el.classList.remove('is-active');
+}
+
+
+
+main();
+
+
 
 const navToggle = document.querySelector('.nav-toggle'); // Otetaan hampulais nappi omaan muuttujaan
 const links = document.querySelector('.links'); // Otetaan linkit omaan muuttujaan
