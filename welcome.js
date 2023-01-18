f52438e6f49c7e075c8b0c87492f8f0414cf0ccb
Tero Keränen <tero-keranen@hotmail.com>
2023-01-18 20:45:44 +0200

    testing if site work on github when I take js.file off

--- welcome.js
+++
@@ -1,7 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle'); // Otetaan hampulais nappi omaan muuttujaan
-const links = document.querySelector('.links'); // Otetaan linkit omaan muuttujaan
-const navCenter = document.querySelector('.nav-center');
-
-navToggle.addEventListener('click', function() {
-        links.classList.toggle('show-links');
-    })
